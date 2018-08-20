83229ca90fe696d95617b5d2f621858a466283ba
lemonjing <932191671@qq.com>
2018-08-20 16:02:49 +0800

    python3

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import sys
 a = None
 b = None
 c = 'dasd'
-print a is None
-print b is None
-print a == b
-print a is b
-print a == None
-print b == None
+print(a is None)
+print(b is None)
+print(a == b)
+print(a is b)
+print(a == None)
+print(b == None)
 
-print sys.getdefaultencoding()
-print type(c)
+print(sys.getdefaultencoding())
+print(type(c))
 
